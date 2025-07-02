8a6ac85ab6dc8a2d70abe81bf39bdbbbd0cfacf3
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-02 11:10:06 +0000

    var

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail="pr9056549@gmail.com"
+var accountPassword="12345"
+accountCity="jaipur"
+
+//accountId=2
+ 
+accountEmail="raj01@gmail.com"
+accountPassword="123321"
+accountCity="Ranchi"
+//console.log(accountId);
+//console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity]);
