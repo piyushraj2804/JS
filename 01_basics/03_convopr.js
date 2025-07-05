0f81efb501f485fcceae603e926682d4e625b664
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-05 16:58:24 +0000

    operation

--- 01_basics/03_convopr.js
+++ 01_basics/03_convopr.js
@@ -10,9 +10,22 @@ let valueInNumber=Number(score)
 
 let isLoggedIn=1
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+let value =3
+let negValue=-value
+//console.log(negValue);
+let str1="hello"
+let str2="piyush"
+let str3=str1+str2
+//console.log(str3)
+let num1, num2, num3
+num1=num2=num3=2+2
+
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
