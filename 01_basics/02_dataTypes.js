36cc99252d8ada1eba8d45edb8f89dea1eb72054
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-05 16:33:22 +0000

    datatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,12 @@
+"use strict";//treat all JS code as newer version
+//alert("hello")
+
+//console.log(3+3)
+//console.log("hitesh")
+
+let name="piyush"
+let age="20"
+let isLoggedIn=false
+let state;
+//number,bigint,string,boolean,null,undefined, symbol,object
+console.log(typeof null)
