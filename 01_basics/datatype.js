8a214c52c0f0f356a14ce6d6398e23c387c27bab
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-13 15:58:03 +0000

    stack and heap

---
+++ 01_basics/datatype.js
@@ -0,0 +1,15 @@
+let youtubename="piyush"
+let anothername=youtubename
+anothername="raj"
+console.log(youtubename)
+console.log(anothername)
+
+let userone={
+    email:"user@google.com",
+    upi:"user@ybl"
+}
+
+let usertwo=userone
+usertwo.email="piyu@google.com"
+console.log(userone.email);
+console.log(usertwo.email);
