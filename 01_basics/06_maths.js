aa24de024e4c124b64e27520e14089b54e08c74a
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-14 17:27:31 +0000

    .

---
+++ 01_basics/06_maths.js
@@ -0,0 +1,11 @@
+const score=400
+
+const bal=new Number(100)
+console.log(bal);
+
+console.log(bal.toString().length)
+console.log(bal.toFixed(2))
+
+const othernum=21.9383
+
+console.log(othernum.toPrecision(3))
