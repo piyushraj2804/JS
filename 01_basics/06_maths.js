ef4dd1f2a628f426bb7ec81926f9f2a97d3360d0
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-17 05:05:18 +0000

    06_maths

--- 01_basics/06_maths.js
+++ 01_basics/06_maths.js
@@ -1,11 +1,34 @@
 const score=400
 
 const bal=new Number(100)
-console.log(bal);
+//console.log(bal);
 
-console.log(bal.toString().length)
-console.log(bal.toFixed(2))
+//console.log(bal.toString().length)
+//console.log(bal.toFixed(2))
 
 const othernum=21.9383
 
-console.log(othernum.toPrecision(3))
+//console.log(othernum.toPrecision(2))
+
+const hundreds=1000000
+//console.log(hundreds.toLocaleString('en-IN'))
+
+
+/////maths
+//console.log(Math)
+//console.log(Math.abs(-4))
+//console.log(Math.round(4.3))
+//console.log(Math.ceil(4.2))//top value
+//console.log(Math.floor(4.9))//bottom value
+//console.log(Math.min(4,6,1,4))
+console.log(Math.random())///random value always between 0 and 1
+console.log(Math.floor(Math.random()*10)+1)
+
+const min=10
+const max=20
+
+Math.random()*(max-min+1)+min
+
+//imp formula
+console.log(Math.floor(Math.random()*(max-min+1)+min))
+//--------------//
