484f48d439387dbd6722dbeef31bbf4deb63c3b8
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-29 04:37:59 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,10 @@
+const myArr=[0,1,2,3,4,5,"piyush"]
+const myher=["thor","apman","ssd"]
+
+const myArr2=new Array(1,2,3,4)
+//console.log(myArr[0])
+
+myArr.push(6)
+myArr.pop()//remove last value
+
+myArr.unshift(9)//add first and shift all
