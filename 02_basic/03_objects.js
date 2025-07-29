d44be7806dd7d7e9ef3a48644abe66194455be33
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-29 05:15:08 +0000

    obj

---
+++ 02_basic/03_objects.js
@@ -0,0 +1,30 @@
+//Object.create
+
+
+const mySym=Symbol("key1")
+
+
+
+const Jsuser={
+    name:"Piyush",
+    [mySym]:"mykey1",
+    age:20,
+    location:"Ranchi",
+    email:"pr9056549@gmail.com"
+}
+
+//console.log(Jsuser.name)
+//console.log(Jsuser["email"])
+//console.log(Jsuser[mySym])
+
+
+Jsuser.email="pr9033"
+Object.freeze(Jsuser)
+
+
+Jsuser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+console.log(Jsuser.greeting())
+
