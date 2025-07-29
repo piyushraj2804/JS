3fb4e297effc1cb8e96af2dd5ffba46ffe33ed29
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-07-29 04:54:09 +0000

    array2

---
+++ 02_basic/02_array.js
@@ -0,0 +1,26 @@
+const marvel_heroes=["thor","iman","spdman"]
+const dc=["spman","flash","batman"]
+//marvel_heroes.push(dc)
+
+//console.log(marvel_heroes)
+//console.log(marvel_heroes[3][1])
+//const all=marvel_heroes.concat(dc)
+//console.log(all)
+
+//const all_new_heroes=[...marvel_heroes,...dc]
+//console.log(all_new_heroes)
+
+//const another_array=[1,2,3,[4,5,6],7,[6,7[4,5]]]
+//const real_arr = another_array.flat(Infinity)
+//console.log(real_arr)
+
+
+
+console.log(Array.isArray("piyush"))
+console.log(Array.from("piyush"))
+console.log(Array.from({name:"piyush"}))
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3))
