e35eb1206a7dc72f2ef9de90b610e0b275c2473f
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-09-29 09:57:56 +0000

    scope

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -3,10 +3,45 @@ let a=300
 if(true){
     let a=10
 const b=20
-console.log("inner:",a)
+//console.log("inner:",a)
 }
 
 
-console.log(a);
+//console.log(a);
 //console.log(b);
 //console.log(c);
+
+function one(){
+    const username="piyush"
+    function two(){
+        const websites="youtube"
+        console.log(username);
+    }
+    //console.log(website);
+two()
+}
+//one()
+
+if(true){
+    const username="piyush"
+    if(username ==="piyush"){
+        const website=" youtube"
+        console.log(username+website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+addone(5)
+function addone(num){
+    return num+1
+
+}
+
+
+
+const addtwo=function(num){
+    return num+2
+}
+addtwo(5)
