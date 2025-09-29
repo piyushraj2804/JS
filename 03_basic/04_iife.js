631f71fca924c1ddafda1b9d6be5b54fcd00cb21
piyushraj2804 <146537372+piyushraj2804@users.noreply.github.com>
2025-09-29 10:23:14 +0000

    iife

---
+++ 03_basic/04_iife.js
@@ -0,0 +1,11 @@
+//immediately invoked function expression(IIFE)
+
+(function chai(){
+    //named iife
+    console.log(`DB CONNECTED`);
+
+})();
+
+( (name)=>  {
+    console.log(`DB CONNECTED TWO ${name}`);
+})('piyush')
